a5bc5037e25cafcbab8cd35b4712ea506047400d
Jagannath Panigrahy <jagannathpanigrahy2012@gmail.com>
2025-07-25 11:44:14 +0530

    refactor: clean up commented-out code and enhance route handling

--- src/app.js
+++ src/app.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const app = express();
 
-app.use("/", (req, res)=>{
-    res.send("Hello, World!");
-})
+// app.use("/hello", (req, res)=>{
+//     res.send("Hello, World!");
+// },)
+
+// app.use("/", (req, res)=>{
+//     res.send("default route");
+// })
+
+
+
+app.get('/hello', (req, res) => {
+    res.send('Hello, World! (GET )');
+});
+
+app.post('/hello', (req, res) => {
+    res.send('Hello, World! (POST)');
+});
+
+// Middleware to log request method and URL
+
+app.get('/', (req, res, next) => {
+    next()
+}, (req,res)=> {
+    res.send('Hello, World! (DEFAULT)');
+    // console.log("Request received at root route")
+});
+ 
+
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
